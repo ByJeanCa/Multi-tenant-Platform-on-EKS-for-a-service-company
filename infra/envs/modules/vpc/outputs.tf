1b9ea907e0d715388ecb239c8e5537913bcd35e9
byjeanca <jo7624822@gmail.com>
2026-02-16 11:45:22 -0600

    feat: Added output for each subnet ID

--- infra/envs/modules/vpc/outputs.tf
+++ infra/envs/modules/vpc/outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "public_subnets_id" {
+  value = [for k in sort(keys(aws_subnet.public)) : aws_subnet.public[k].id]
+}
+
+output "private_subnets_id" {
+  value = [for k in sort(keys(aws_subnet.private)) : aws_subnet.private[k].id]
+}
+
+output "db_subnets_id" {
+  value = [for k in sort(keys(aws_subnet.db)) : aws_subnet.db[k].id]
+}
