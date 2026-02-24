05e82e8d1e94cab49a33c7dc2bb4542598b1df1c
byjeanca <jo7624822@gmail.com>
2026-02-24 15:02:09 -0600

    feat(k8s): Create correct namespace and serviceaccount to ensure irsa connection

--- infra/envs/modules/eks/outputs.tf
+++ infra/envs/modules/eks/outputs.tf
@@ -9,3 +9,11 @@ output "eks_name" {
 output "eks_oidc_issuer" {
   value = aws_eks_cluster.main.identity[0].oidc[0].issuer
 }
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_certificate_authority" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
