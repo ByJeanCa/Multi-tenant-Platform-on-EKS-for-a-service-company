46e7734d7d746cd73436f37794244eba0baa70ce
byjeanca <jo7624822@gmail.com>
2026-03-10 13:06:31 -0600

    feat(eks): Create Policies, roles and podIdentitys for external-dns-controller

--- infra/envs/modules/eks/main.tf
+++ infra/envs/modules/eks/main.tf
@@ -189,3 +189,65 @@ resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "eks-pod-identity-agent"
 }
+
+
+resource "aws_iam_policy" "external_dns" {
+  name = "external_dns_iam_policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "route53:ChangeResourceRecordSets"
+          ],
+          "Resource" : [
+            "arn:aws:route53:::hostedzone/*"
+          ]
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "route53:ListHostedZones",
+            "route53:ListResourceRecordSets"
+          ],
+          "Resource" : [
+            "*"
+          ]
+        }
+      ]
+  })
+}
+
+resource "aws_iam_role" "external-dns-controller" {
+  name = "external-dns-controller"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "pods.eks.amazonaws.com"
+            },
+            "Action": [
+                "sts:TagSession",
+                "sts:AssumeRole"
+            ]
+        }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach-dns-controller-policy" {
+   policy_arn = aws_iam_policy.external_dns.arn
+  role       = aws_iam_role.external-dns-controller.name
+}
+
+resource "aws_eks_pod_identity_association" "external-dns-controller" {
+  cluster_name = aws_eks_cluster.main.name
+  namespace = "kube-system"
+  service_account = "external-dns-controller"
+  role_arn = aws_iam_role.external-dns-controller.arn
+}
