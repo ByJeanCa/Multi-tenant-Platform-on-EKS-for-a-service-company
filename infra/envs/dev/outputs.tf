be40855b008b073bba9dc87490cb8d6b024782dc
byjeanca <jo7624822@gmail.com>
2026-03-12 12:36:37 -0600

    chore(terraform): expose required ARNs and IDs through outputs for k8s

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -1,3 +1,15 @@
 output "db_endpoint" {
   value = module.db.db_endpoint
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "rds_secret_arn" {
+  value = module.db.db_master_secret_arn
+}
+
+output "cert_arn" {
+  value = module.cert.cert_arn
+}
--- infra/envs/modules/cert/outputs.tf
+++ infra/envs/modules/cert/outputs.tf
@@ -1,3 +1,7 @@
 output "nameservers" {
   value = aws_route53_zone.web_domain.name_servers
 }
+
+output "cert_arn" {
+  value = aws_acm_certificate.cert.arn
+}
